eaca493f7651e6a904e503f17414f3115b6b8fff
Yash Agarwal <yash23.agarwal@gmail.com>
2018-10-27 02:05:50 +0530

    Added all major activities

--- app/src/main/java/com/example/hackcbs/CropRecyclerViewAdapter.java
+++ app/src/main/java/com/example/hackcbs/CropRecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.hackcbs;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -7,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,10 +17,10 @@ import android.widget.TextView;
 public class CropRecyclerViewAdapter extends RecyclerView.Adapter<CropRecyclerViewAdapter.MyViewHolder> {
 
         CardView cardView;
+        Context context;
     private Crop[] presentCrops;
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
 
         CardView cardView;
         ImageView cropPhotoImageView;
@@ -30,15 +33,24 @@ public class CropRecyclerViewAdapter extends RecyclerView.Adapter<CropRecyclerVi
         }
     }
 
-    public CropRecyclerViewAdapter(Crop[] presentCrops) {
+    public CropRecyclerViewAdapter(Crop[] presentCrops, Context context) {
         this.presentCrops = presentCrops;
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
         CardView cropCardView = (CardView) LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.crop_cardview, viewGroup, false);
+        cropCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,CropStatusActivity.class);
+                intent.putExtra("Crop Name", presentCrops[i].getName());
+                context.startActivity(intent);
+            }
+        });
         MyViewHolder vh = new MyViewHolder(cropCardView);
         return vh;
     }
--- app/src/main/java/com/example/hackcbs/MainActivity.java
+++ app/src/main/java/com/example/hackcbs/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.hackcbs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
 //        TODO get crops from database
         Crop presentCrops[] = {new Crop("Wheat",R.drawable.wheat)};
-        adapter = new CropRecyclerViewAdapter(presentCrops);
+        adapter = new CropRecyclerViewAdapter(presentCrops, MainActivity.this);
         recyclerView.setAdapter(adapter);
+
+        FloatingActionButton addNewCrop = (FloatingActionButton) findViewById(R.id.crop_add_button);
+        addNewCrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNewCropActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
